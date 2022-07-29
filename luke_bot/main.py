00b3fc2fcd101fa7c817d4fb5a00eeb642c9cdba
Charlie Wilson <charles.ep.wilson@gmail.com>
2022-07-29 20:48:54 +0100

    Rearrange

--- luke_bot/main.py
+++ luke_bot/main.py
@@ -4,5 +4,11 @@ from luke_bot.discord_bot import bot
 
 
 def main():
+    environment_variables = ('GG_TOKEN', 'DISCORD_TOKEN', 'DISCORD_CHANNEL_ID')
+
+    for var in environment_variables:
+        if os.getenv(var) is None:
+            raise RuntimeError(f'Environment variable {var} not set.')
+
     discord_token = os.getenv('DISCORD_TOKEN')
     bot.run(discord_token)
