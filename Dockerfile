2a33f53b0e3861f5745f9499e7d58488b966489d
Charlie Wilson <charles.ep.wilson@gmail.com>
2022-07-29 20:46:43 +0100

    Update Dockerfile and check for env vars.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM python:3.10-slim
 
-ENV GG_TOKEN
-ENV DISCORD_TOKEN
-ENV DISCORD_CHANNEL_ID
-
 COPY . .
 
 RUN pip install -r requirements.txt
--- luke_bot/__init__.py
+++ luke_bot/__init__.py
@@ -1,8 +1,14 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 from luke_bot.main import main # noqa
 
+environment_variables = ('GG_TOKEN', 'DISCORD_TOKEN', 'DISCORD_CHANNEL_ID')
+
+for var in environment_variables:
+    if os.getenv(var) is None:
+        raise RuntimeError(f'Environment variable {var} not set.')
 
 if __name__ == '__main__':
     main()
