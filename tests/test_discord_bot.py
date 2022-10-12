bcc642a62fed7d1685c61d5687bf0406b6bb02cb
Charlie Wilson <charles.ep.wilson@gmail.com>
2022-10-12 21:07:42 +0100

    Start on new test approach

--- tests/test_discord_bot.py
+++ tests/test_discord_bot.py
@@ -1,5 +1,7 @@
+import os
+
 import pytest
-import discord.ext.test as dpytest
+import discord
 
 from luke_bot.discord_bot import same_update
 from . import test_data as test
@@ -11,15 +13,12 @@ def test_same_update():
 
 
 @pytest.mark.asyncio
-async def test_send_to_luke_updates(bot):
+async def test_send_to_luke_updates():
     # this test should somehow check that the bot sends appropriate messages into the correct channel
     # and that it correctly knows when to post a new update or skip repeats
     # should probably mock the smashgg interface though for consistent tests
     # maybe just have a list of dummy responses that feed into it with known expected results
-    assert True
-
-
-@pytest.mark.asyncio
-async def test_about(bot):
-    await dpytest.message('!about')
-    assert dpytest.get_message().content == "I report updates on Luke"
+    tester = discord.Client()
+    await tester.login(os.getenv("DISCORD_TESTER_TOKEN"))
+    channel = tester.get_channel(int(os.getenv("DISCORD_CHANNEL_ID")))
+    assert channel.name == "luke-updates"
